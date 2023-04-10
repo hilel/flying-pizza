b6ccdee85d3842d5b0d44eb78a44528cb02edff2
Hilel Elizarov <hilel1011@gmail.com>
2023-04-10 20:00:20 +0300

    added environments files

---
+++ apps/admin/src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://localhost:3333' // 'http://127.0.0.1:5001/proven-gasket-88888888/us-central1'
+};
---
+++ apps/admin/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://proven-gasket-88888888.web.app' // apiBaseUrl: 'https://europe-west1-proven-gasket-88888888.cloudfunctions.net'
+};
