bca3ceb9519b3ab80c9b8c9af813e82d4c6cc704
Hilel Elizarov <hilel1011@gmail.com>
2023-04-15 19:00:27 +0300

    removed backend app from workspace

--- apps/backend/src/app/pizzas/pizzas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PizzasService } from './pizzas.service';
-import { PizzasController } from './pizzas.controller';
-
-@Module({
-  controllers: [PizzasController],
-  providers: [PizzasService],
-})
-export class PizzasModule {}
